57b8c9914c2d37fd281db041ab1bd788de9207e1
WhiteeI <958237506@qq.com>
2019-05-06 11:14:17 +0800

    Fibonacci非递归

--- A/A/A.c
+++ A/A/A.c
@@ -1,10 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-int main() {
-	int i = 011;
-	printf("%d\n", i);
+long long* Fibonacci(size_t n) {
+	if (n == 0) {
+		return NULL;
+	}
+	long long* fibarray = (long long*)malloc(n * 8 + 1);
+	fibarray[0] = 0;
+	fibarray[1] = 1;
+	for (size_t i = 2; i < n; ++i) {
+		fibarray[i] = fibarray[i - 1] + fibarray[i - 2];
+	}
+	return fibarray;
+}
 
+int main() {
+	long long* fib = Fibonacci(10);
+	printf("%d",fib[9]);
+	free(fib);
 	system("pause");
 	return 0;
 }
